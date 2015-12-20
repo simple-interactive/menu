15b9f3215e4444f2d4d29786c380f0d37ea1c690
Simple Live <live@simple.com>
2015-12-20 06:50:46 +0300

    Updated conf

--- gruntfile.js
+++ gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
 
     var config = {
         prod : {
-            endpoint: 'http://192.168.1.103:82',
+            endpoint: 'http://185.70.109.10:81',
             token: '27c2f4bb1748ae677a4bf6c160122ed4'
         },
         dev: {
